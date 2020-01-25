5eee9b08bbdb5689051b55103db0482ee89101bf
sjoerdermshaus <sjoerdermshaus@gmail.com>
2020-01-25 09:57:07 +0100

    class added

--- test.py
+++ test.py
@@ -1,8 +1,22 @@
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame(dict(A=[1, 2, 3]))
-print(df)
 
-a = np.array([4, 5, 6])
-print(a)
+class C:
+    def __init__(self):
+        self.df = pd.DataFrame(dict(A=[1, 2, 3]))
+    
+    def print_df(self):
+        print(self.df)
+
+    def print_array(self):
+        a = np.array([4, 5, 6])
+        print(a)
+
+def main():
+    c = C()
+    c.print_df()
+    c.print_array()
+
+if __name__ == '__main__':
+    main()
